eee6e9b7aac42d294023a86bf9878c71aad9bf17
SonHip <sonhipkmhd@gmail.com>
2025-01-13 15:51:15 +0700

    remove unused code to build

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import AboutUsSection from "./pages/about-us-section";
 import CountDownSection from "./pages/count-down-section";
 import EventDetailsSection from "./pages/event-details-section";
 import FooterSection from "./pages/footer-section";
-import GallerySection from "./pages/gallery-section";
 import HeroSection from "./pages/hero-section";
 import PeopleSection from "./pages/people-section";
 import ThankYouSection from "./pages/thank-you-section";
