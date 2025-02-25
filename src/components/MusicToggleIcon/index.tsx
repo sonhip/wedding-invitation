a920d91972018627d996388d2f35df136d6cf99f
SonHip <sonhipkmhd@gmail.com>
2025-02-25 22:09:09 +0700

    refactor: remove unused CSS and improve music toggle functionality

--- src/components/MusicToggleIcon/index.tsx
+++ src/components/MusicToggleIcon/index.tsx
@@ -1,82 +1,51 @@
-import { useToast } from "@/hooks/use-toast";
-import React, { useEffect, useState } from "react";
-import { HiMusicNote, HiVolumeOff } from "react-icons/hi"; // Icon của react-icons
+import React, { useRef, useState } from "react";
+import { HiMusicNote, HiVolumeOff } from "react-icons/hi";
 
 const MusicToggleIcon: React.FC = () => {
-  const [isPlaying, setIsPlaying] = useState(false); // Trạng thái nhạc (mặc định là không phát)
-  const [audio] = useState(
-    new Audio(
-      "https://res.cloudinary.com/db8mh2s66/video/upload/v1740411565/wedding-images/fbkdkzhxkrnvkgu1idii.mp3"
-    )
-  ); // Đường dẫn đến file nhạc
-  const { toast } = useToast();
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
 
-  // Hàm toggle nhạc (mở/tắt)
   const toggleMusic = () => {
-    if (!isPlaying) {
-      // Nếu nhạc chưa được bật, cố gắng phát nhạc khi người dùng nhấn vào biểu tượng
-      audio.play().catch((error) => {
-        console.error("Error playing audio: ", error);
-        // toast({
-        //   title: "Lỗi phát nhạc",
-        //   description: "Có lỗi khi phát nhạc!",
-        //   className: "bg-red-500 text-white rounded-lg p-4 shadow-lg", // Tailwind class cho lỗi
-        //   duration: 2000,
-        // });
-      });
-
-      toast({
-        title: "Nhạc đã bật",
-        description: "Nhạc đang phát!",
-        className: "bg-green-500 text-white rounded-lg p-4 shadow-lg", // Tailwind class cho thành công
-        duration: 2000,
-      });
+    if (!audioRef.current) return;
+    if (isPlaying) {
+      audioRef.current.pause();
     } else {
-      // Nếu nhạc đang phát, dừng nhạc
-      audio.pause();
-      toast({
-        title: "Nhạc đã tắt",
-        description: "Nhạc đã được tắt!",
-        className: "bg-red-500 text-white rounded-lg p-4 shadow-lg", // Tailwind class cho thành công
-        duration: 2000,
+      audioRef.current.play().catch((error) => {
+        console.error("Error playing audio: ", error);
       });
     }
-
-    // Đảo trạng thái của nhạc
     setIsPlaying((prev) => !prev);
   };
 
-  // Tự động phát nhạc nếu cần sau khi người dùng nhấn vào biểu tượng
-  useEffect(() => {
-    if (isPlaying) {
-      audio.play().catch((error) => {
-        console.error("Error playing audio: ", error);
-        // toast({
-        //   title: "Lỗi phát nhạc",
-        //   description: "Có lỗi khi phát nhạc!",
-        //   className: "bg-red-500 text-white rounded-lg p-4 shadow-lg", // Tailwind class cho lỗi
-        //   duration: 2000,
-        // });
-      });
-    } else {
-      audio.pause();
-    }
-
-    return () => {
-      audio.pause(); // Dừng nhạc khi component bị unmount
-    };
-  }, [isPlaying, audio]); // Khi isPlaying thay đổi, phát nhạc
-
   return (
     <div
+      className="fixed bottom-4 z-[999] left-4 p-4 bg-wedding-deep rounded-full shadow-lg cursor-pointer transition-all duration-300 flex items-center space-x-2"
       onClick={toggleMusic}
-      className="fixed bottom-4 left-4 p-4 bg-wedding-deep rounded-full shadow-lg cursor-pointer transition-all duration-300"
     >
+      <div
+        className={`bars-container ${
+          isPlaying ? "bars-active" : "bars-paused"
+        }`}
+      >
+        {[...Array(5)].map((_, i) => (
+          <div
+            key={i}
+            className="bar"
+            style={{ animationDelay: `${i * 0.1}s` }}
+          ></div>
+        ))}
+      </div>
       {isPlaying ? (
         <HiMusicNote className="text-white text-3xl" />
       ) : (
         <HiVolumeOff className="text-white text-3xl" />
       )}
+      <audio
+        loop
+        ref={audioRef}
+        src="https://res.cloudinary.com/db8mh2s66/video/upload/v1740411565/wedding-images/fbkdkzhxkrnvkgu1idii.mp3"
+        preload="auto"
+      />
     </div>
   );
 };
