0afb69ae5f6c225b66e1eed35995360e846fac1e
SonHip <sonhipkmhd@gmail.com>
2025-11-06 13:52:59 +0700

    feat: update image paths in Open Graph meta tags and constants; remove unused preview image

--- src/config/const.ts
+++ src/config/const.ts
@@ -163,18 +163,18 @@ export const STAGES: Stage[] = [
     title: "Bắt đầu từ một ngày mùa xuân nào đó",
     description:
       "Cuộc hội ngộ của hai mảnh ghép đơn lẻ. Chúng mình đã vô tình tìm thấy nhau giữa biển người bao la rộng lớn. Và dù có những xa lạ và bỡ ngỡ ban đầu, chúng mình vẫn quyết định cùng cho nhau một cơ hội để nán lại tìm hiểu người kia thêm một chút. Và câu chuyện tình này bắt đầu..",
-    image: "/public/images/white-x.jpg",
+    image: "/images/white-x.jpg",
   },
   {
     title: "Cột mốc đáng nhớ",
     description:
       'Quyết định quan trọng. Giống như mọi bản nhạc đều sẽ có lúc thăng trầm, câu chuyện sẽ có lúc lên xuống, chúng mình đã cùng nhau trải qua những khoảnh khắc vui, buồn, giận, hờn bên nhau. Và rồi ngày đó đã đến, sau thời gian cùng nhau điều chỉnh, "mài giũa", hai mảnh ghép quyết định sẽ không xuôi theo dòng chảy của riêng mình nữa. Chúng mình quyết định dừng chân để là bến đỗ cuối cùng của nhau...',
-    image: "/public/images/white-y.jpg",
+    image: "/images/white-y.jpg",
   },
   {
     title: "30/11/2025 và sau nữa",
     description:
       'Ngày chung đôi. Câu chuyện khởi đầu vào mùa xuân, nên chúng mình muốn viết nên một cái kết hạnh phúc vào mùa đông. Để mùa xuân tiếp theo, chúng mình sẽ cùng nhau viết nên một câu chuyện khác, nhưng giờ với một vai trò mới, là "Vợ", là "Chồng". Vào ngày 30/11/2025 tới này, mong bạn đến chung vui và cùng chứng kiến cái kết hạnh phúc cho câu chuyện chúng mình, và cùng nhau mở ra một hành trình mới nhé!',
-    image: "/public/images/white-xy3.jpg",
+    image: "/images/white-xy3.jpg",
   },
 ];
